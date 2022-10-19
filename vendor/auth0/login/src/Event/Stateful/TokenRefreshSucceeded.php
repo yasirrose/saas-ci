79039dbf1214720489d7f00a32badde49d3ece59
Yasir Shaukat <yasirzinbox@gmail.com>
2022-10-19 19:42:19 +0500

    Delete vendor directory

--- vendor/auth0/login/src/Event/Stateful/TokenRefreshSucceeded.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Auth0\Laravel\Event\Stateful;
-
-final class TokenRefreshSucceeded extends \Auth0\Laravel\Event\Auth0Event implements \Auth0\Laravel\Contract\Event\Stateful\TokenRefreshSucceeded
-{
-}
