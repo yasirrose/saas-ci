e2d8629dad4fd1632737378b0749d7b2e34b571b
Yasir Shaukat <yasirzinbox@gmail.com>
2022-10-19 19:42:03 +0500

    Delete storage directory

--- storage/framework/views/969e94722cfcfbbda92369f1270d599019bf398c.php
+++
@@ -1,3 +0,0 @@
-<div class="search">
-    <input type="text" class="search-input" placeholder="<?php echo e(__('translation::translation.search')); ?>" name="<?php echo e($name); ?>" value="<?php echo e($value); ?>">
-</div><?php /**PATH C:\xampp\htdocs\saas\resources\views/vendor/translation/forms/search.blade.php ENDPATH**/ ?>
--- storage/framework/views/ee9bf7751d8217fb7b7f2907a404b33f43d1c0f9.php
+++
@@ -1,8 +0,0 @@
-<div>
- If you look to others for fulfillment, you will never truly be fulfilled. 
-
-(child component)
-<input type="text" wire:model="name">  <?php echo e($name); ?>
-
-</div>
-<?php /**PATH C:\xampp\htdocs\saas\resources\views/livewire/child-component.blade.php ENDPATH**/ ?>
