293ad5c36479f40ee3b3c618e8ddf57ccc5603a7
Yasir Shaukat <yasirzinbox@gmail.com>
2022-10-19 19:45:39 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/views/home.blade.php",
+        "./resources/views/welcome.blade.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
